048266520ddb26787acf9ba761e88adeda03c0dc
songzhanlong <song_zhanlong@hotmail.com>
2018-04-17 22:14:32 +0800

    集成测试自动配置

--- src/test/java/com/example/demo/ReadingListApplicationTests.java
+++ src/test/java/com/example/demo/ReadingListApplicationTests.java
@@ -1,16 +1,34 @@
 package com.example.demo;
 
+import com.example.demo.dao.BookRepository;
+import com.example.demo.domain.Book;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ReadingListApplicationTests {
+    @Autowired
+    private BookRepository bookRepository;
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+        Book book = new Book();
+        book.setReader("tom");
+        book.setAuthor("jerry");
+        book.setTitle("helloworld");
+        bookRepository.save(book);
+        List<Book> bookList = bookRepository.findByReader("tom");
+        Book book1 = bookList.get(0);
+        assertEquals("jerry", book1.getAuthor());
+        assertEquals("helloworld", book1.getTitle());
+    }
 
 }
